bd2a593e6fb437f17e978d91e9be0390f31fdf45
bintangshada <bintangshada45@gmail.com>
2025-09-24 03:01:43 +0700

    feat: list in articles

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,5 +1,30 @@
-export default function ArticlesPage(){
+import Link from "next/link"
+import prisma from "../lib/prisma"
+
+async function getArticles() {
+    const blogs = await prisma.article.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+    return blogs;
+}
+
+export default async function ArticlesPage(){
+    const blogs = await getArticles();
     return(
-        <h1 className="flex justify-center">List Artikel</h1>
+        <div>
+            <h1 className="flex justify-center">List Artikel</h1>
+            <ul className="list-inside list-disc">
+                {blogs && blogs.length > 0 ? blogs.map((blog) => (
+                    <li key={blog.id}>
+                        <Link href={`/articles/${blog.slug}`}>
+                            <h3 className="inline">{blog.title} - </h3>
+                            <p className="inline text-gray-500">{blog.createdAt.toLocaleDateString()}</p>
+                        </Link>        
+                    </li>
+                )) : <li>No Blogs found</li> }
+            </ul>
+        </div>
     )
 }
