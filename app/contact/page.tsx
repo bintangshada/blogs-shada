3840d2476e2be502b6d0cdaadc0e380616e0f7aa
bintangshada <bintangshada45@gmail.com>
2025-09-24 03:09:48 +0700

    feat: contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,10 @@
 export default function ContactPage(){
     return(
-        <h1 className="flex justify-center">Contact Me</h1>
+        <div className="max-w-200">
+            <div>
+                <h1 className="flex justify-center my-4">Contact Me</h1>
+                <a className="flex justify-center" href="mailto:bintangshada45@gmail.com">bintangshada45@gmail.com</a>
+            </div>
+        </div>
     )
 }
