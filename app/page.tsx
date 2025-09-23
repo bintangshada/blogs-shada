c7535a3d26ffcb41e61d79a7d04327d99e6e4fdf
bintangshada <bintangshada45@gmail.com>
2025-09-24 02:49:13 +0700

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
   return (
         <div className="container max-w-200">
           <div className="hero">
-            <h1 className="flex justify-center">Hello WElcome to my Portofolio</h1>
+            <h1 className="flex justify-center">Hello Welcome to my Blogs</h1>
             <br />
             <p>My name is Bintang Shada Kawibya Putra but you can call me Shada, a little bit about myself—to be honest, I don&apos;t really know what I want to focus on right now (dd23/MM09/yy25), so I&apos;m trying everything, but I tend to get bored with things easily. That&apos;s why I created this website, which might become a story about my life journey. I made it simple, just the way I want it, so don&apos;t expect too much. And I hope I can write a lot of blogs here too, hehe.</p>
           </div>
