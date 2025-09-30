8f046609238858ad9b0e58f7698714b4241ccd26
bintangshada <bintangshada45@gmail.com>
2025-10-01 03:34:34 +0700

    feat: api login

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,26 @@
+import prisma from "@/app/lib/prisma";
+import bcrypt from 'bcrypt';
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export async function POST(req: Request){
+    const { email, password } = await req.json()
+
+    const user = await prisma.user.findUnique({
+        where: {email}
+    })
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return NextResponse.json(
+            {error: "User or password not found"},
+            {status: 404}
+        )
+    }
+    
+    const token = jwt.sign({userId : user.id, email : user.email}, process.env.JWT_SECRET as string, {expiresIn: "24h"})
+
+    return NextResponse.json(
+        {token, user: {id: user.id, email: user.email}},
+        {status: 400}
+    )
+}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-
+import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
- async function main(){
+async function main(){
     try {
         await prisma.article.createMany({
             data: [
@@ -10,11 +10,23 @@ const prisma = new PrismaClient();
                 { title: "Artikel Kedua", slug: "artikel-kedua", content: "Ini isi artikel kedua" }
             ]
         })
+        if (!process.env.PASSWORD) {
+            throw new Error("password is not defined");
+        }
+        const password = await bcrypt.hash(process.env.PASSWORD, 10);
+        
+        await prisma.user.create({
+            data: { 
+                name: `${process.env.NAME}`, 
+                email: `${process.env.EMAIL}`, 
+                password: password
+            }
+        })
 
         await prisma.$disconnect();
     } catch (error) {
         console.error(error);
     }
- }
+}
 
- main();
+main();
