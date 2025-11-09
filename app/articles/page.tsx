77ec7df9da5573f3e9ec6a8ee57766d214ef4bdf
bintangshada <bintangshada45@gmail.com>
2025-11-09 20:47:28 +0700

    fix: articles fetch

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link"
 import prisma from "../lib/prisma"
 
+interface Blogs {
+  id: string;
+  slug: string;
+  title: string;
+  createdAt: Date;
+}
+
 async function getArticles() {
-    const blogs = await prisma.article.findMany({
-        orderBy: {
-            createdAt: 'desc'
-        }
-    })
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/articles`, {
+        cache: "no-store"
+    });
+      const blogs = await res.json();
     return blogs;
 }
 
@@ -16,11 +22,11 @@ export default async function ArticlesPage(){
         <div className="max-w-200">
             <h1 className="flex justify-center mb-4">List Artikel</h1>
             <ul className="list-inside list-disc">
-                {blogs && blogs.length > 0 ? blogs.map((blog) => (
+                {blogs && blogs.length > 0 ? blogs.map((blog : Blogs) => (
                     <li key={blog.id}>
                         <Link href={`/articles/${blog.slug}`}>
                             <h2 className="inline">{blog.title} - </h2>
-                            <p className="inline text-gray-500">{blog.createdAt.toLocaleDateString()}</p>
+                            <p className="inline text-gray-500">{new Date(blog.createdAt).toLocaleDateString()}</p>
                         </Link>        
                     </li>
                 )) : <li>No Blogs found</li> }
